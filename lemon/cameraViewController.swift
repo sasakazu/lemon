728d5cbcdf4419a39b73c211ff2488eea12a8859
sasakazu <sasakura.company@gmail.com>
2018-05-23 20:03:27 +0900

    camera file add

---
+++ lemon/cameraViewController.swift
@@ -0,0 +1,27 @@
+//
+//  cameraViewController.swift
+//  lemon
+//
+//  Created by 笹倉 一也 on 2018/05/23.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class cameraViewController: UIViewController {
+
+    
+    var device: AVCaptureDevice!
+    var session: AVCaptureSession!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+  
+    }
+
+    
+}
